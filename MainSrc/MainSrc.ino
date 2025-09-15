3fdc1320cae73531c72d0a310a46b0c1ce946b7e
David Purty <63737751+Davidpurty@users.noreply.github.com>
2025-09-16 01:44:39 +0530

    Update MainSrc.ino

--- MainSrc/MainSrc.ino
+++ MainSrc/MainSrc.ino
@@ -1,8 +1,12 @@
 
 
 #include "BatteryMonitor.h"
+#include "Button.h"
+#include "Led.h"
 
-BatteryMonitor battery;
+BatteryMonitor battery(A0,A1,A2);
+Button mainbutton(1);
+Led LCD_display(2);
 void setup()
 {
   Serial.begin(115200);
@@ -19,7 +23,12 @@ void loop()
   unsigned long chars;
   unsigned short sentences, failed;
   Serial.println("test done");
-  Serial.println("Battery voltage is : "+ battery.getCell1());
+  Serial.println("Battery voltage is : ");
+  Serial.print(battery.getCell1());
+  Serial.println("Battery 2 voltage:");
+  Serial.println(battery.getCell2());
+  Serial.println("Lcd display ");
+
 
   // For one second we parse GPS data and report some key values
   
