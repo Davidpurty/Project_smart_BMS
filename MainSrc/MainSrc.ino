f89b2d2b96887d383f6cfa6bea1c556f5676841b
david.purty <davidpurty16@gmail.com>
2025-09-20 13:11:14 +0530

    Adding additional files for lcd display

--- MainSrc/MainSrc.ino
+++ MainSrc/MainSrc.ino
@@ -1,17 +1,24 @@
 
+#include <Arduino.h>
+#include <Wire.h>
 
 #include "BatteryMonitor.h"
 #include "Button.h"
 #include "Led.h"
+#include "LiquidCrystal_PCF8574.h"
+
 
 BatteryMonitor battery(A0,A1,A2);
 Button mainbutton(1);
 Led LCD_display(2);
+
+LiquidCrystal_PCF8574 lcd(0x27);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 void setup()
 {
   Serial.begin(115200);
   
-  
+  Wire.begin();
+  Wire.beginTransmission(0x27);
   Serial.print("Simple TinyGPS library v. "); 
   Serial.println("by Mikal Hart");
   Serial.println();
@@ -28,6 +35,7 @@ void loop()
   Serial.println("Battery 2 voltage:");
   Serial.println(battery.getCell2());
   Serial.println("Lcd display ");
+  
 
 
   // For one second we parse GPS data and report some key values
